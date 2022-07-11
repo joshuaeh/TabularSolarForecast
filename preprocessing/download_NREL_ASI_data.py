3737fd923388ecc3e48cbeed0ccdc015f6bfa6e1
joshuaeh <jhammond02@gmail.com>
2022-07-11 14:53:23 -0500

    renale download_ASI_data to download_NREL_ASI_data

---
+++ preprocessing/download_NREL_ASI_data.py
@@ -0,0 +1,11 @@
+"""
+Download zip files and organize into project. Source url for ASI-16 : https://midcdmz.nrel.gov/tsi/SRRLASI/YYYY/YYYYMMDD.zip
+"""
+
+# Imports
+
+# Configuration
+
+# Classes and functions
+
+# Script
