558068563a5d22a53080c64163aab26c8a353b2f
joshuaeh <jhammond02@gmail.com>
2022-07-11 14:52:56 -0500

    Create download_NREL_TSI_data.py

---
+++ preprocessing/download_NREL_TSI_data.py
@@ -0,0 +1,11 @@
+"""
+Download zip files and organize into project. Source url for TSI-880 : https://midcdmz.nrel.gov/tsi/SRRL/YYYY/YYYYMMDD.zip
+"""
+
+# Imports
+
+# Configuration
+
+# Classes and functions
+
+# Script
