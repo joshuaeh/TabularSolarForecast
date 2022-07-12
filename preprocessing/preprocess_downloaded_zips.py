fdd013335903b29682d1663d1a6d620c83f8253d
joshuaeh <jhammond02@gmail.com>
2022-07-11 21:58:24 -0500

    add comment with initial date

---
+++ preprocessing/preprocess_downloaded_zips.py
@@ -0,0 +1,21 @@
+"""
+This script will:
+- sub-folder by year
+- Unzip all zipped files
+- rename all images
+- take an accounting of covered times
+- mask images give a cute smiley face when complete
+"""
+
+# imports
+import os
+import zipfile
+import cv2
+import glob
+
+# functions
+def unzip_and_group_by_year():
+    NREL_DATA_Directory = os.path.join(os.path.dirname(__file__), "..","data","NREL")
+    for directory in []
+
+# script
