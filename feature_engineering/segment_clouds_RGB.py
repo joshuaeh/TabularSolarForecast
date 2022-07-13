1c9736ca2b5009ec4479ecacf7c940662f31adaf
joshuaeh <jhammond02@gmail.com>
2022-07-13 12:40:44 -0500

    add segmentation script

---
+++ feature_engineering/segment_clouds_RGB.py
@@ -0,0 +1,20 @@
+"""
+Evaluate how different algorithms evaluate segment clouds
+"""
+
+# imports
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+from torch import nn
+from torch.functional import F
+from copy import copy
+
+# declarations
+## directories for reference images
+###SWIMSEG
+
+###
+# Functions
+
+# Script
