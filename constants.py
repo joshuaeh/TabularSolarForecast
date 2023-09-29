d9520b80bf4d930518ad4d79d5abad97ee60820a
joshuaeh <jhammond02@gmail.com>
2023-09-29 12:58:08 -0500

    remove sensitive data from constants

--- constants.py
+++ constants.py
@@ -1,5 +1,11 @@
+#!/usr/bin/env python3
+# Author: Joshua Hammond
+# Static parameters for the project
+
 import os
 import datetime
+
+import config
 
 # TODO make API tokens and other specifications in another file called "auth" or something similar that is not commited
 # TODO make file structure standardized
@@ -8,19 +14,13 @@ import datetime
 # Directories
 # HOME = os.environ["HOME"]
 # WORK = os.environ["WORK"]
-# DATA_PATH = os.path.join("/work", "08940", "joshuaeh", "shared", "joint_data.csv")
-# SOLARPROPHET_PATH = os.path.join(WORK, "projects", "SolarProphet")
 SOLARPROPHET_PATH = os.getcwd()
 JOINT_DATA_H5_PATH = os.path.join(SOLARPROPHET_PATH, "data", "joint_data.h5")
 JOINT_DATA_CSV_PATH = os.path.join(SOLARPROPHET_PATH, "data", "joint_data.csv")
 DATA_PATH = JOINT_DATA_CSV_PATH
 # Neptune connection
-NEPTUNE_PROJECT = "HorizonPSE/SP-tab"
-NEPTUNE_TOKEN = "eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiIxNTBhNmQ1Ny04MjAzLTQ2ZjUtODA2MC0yNDllNWUxOWE2ZjkifQ=="
-NEPTUNE_FEATURE_ABLATION_TAG = "Ablation Study"
-NEPTUNE_FEATURE_ABLATION_IDS = [f"SPTAB-{i}" for i in range(581, 1093)]
-NEPTUNE_PCA_TAG = "PCA"
-NEPTUNE_PCA_IDS = [f"SPTAB-{i}" for i in range(1150, 1273)]
+NEPTUNE_PROJECT = config.NEPTUNE_PROJECT
+NEPTUNE_TOKEN = config.NEPTUNE_TOKEN
 NEPTUNE_CACHE_PATH = os.path.join(SOLARPROPHET_PATH, "data", "neptune_cache.h5")
 
 # Misc Saved results and and caches
@@ -37,7 +37,7 @@ CV1_DATE = datetime.date(2020,9,27)
 CV2_DATE = datetime.date(2021,9,27)
 END_DATE = datetime.date(2022,9,27)
 
-CSV_COLS = ["dateTime",
+DATA_COLS = ["dateTime",
         # Irradiance    
         'GHI', 'DNI','DHI',
         # Clouds (camera - related)
@@ -187,7 +187,7 @@ FEATURE_GROUPS = {
             'Flag: Sun not visible', 'Flag: Sun on clear sky', 'Flag: Parts of sun covered',
             'Flag: Sun behind clouds, bright dot visible', 'Flag: Sun outside view', 'Flag: No evaluation',
         ]
-    } 
+    } # TODO add future features
 # TODO use .ANONYMOUS as an argument when we are debugging
 # TODO save model weights, go through Neptune best practices
 RESPONSE_FEATURES = {  # to compare in a standardized manner, we should always include the value being predicted as a feature. We will make sure to do that here
@@ -210,7 +210,6 @@ GROUP_ABBREVIATIONS = {
     "Time Milestones" : "TM",
     "Trig Time Milestones" : "TTM",
     "Clear Sky" : "CS",
-    "Future Clear Sky" : "FCS",
     "Prev Hour Stats" : "L60Stats",
     "Meteorological Measurements" : "MET",
     "ASI-16" : "ASI"
@@ -218,3 +217,31 @@ GROUP_ABBREVIATIONS = {
 GROUPS_ORDER = ["Irradiance", "Decomposed Irradiance", "Lagged 10 min GHI", "Lagged 10 min Decomposed Irradiance", "Time of Day", "Trig Time of Day", "Time of Year",
     "Trig Time of Year", "Time Milestones", "Trig Time Milestones", "Clear Sky", "Prev Hour Stats", "Meteorological Measurements", "ASI-16"]
 RESPONSE_ORDER = ["GHI", "CSI GHI", "cs_dev t ghi", "Delta GHI", "Delta CSI GHI"]
+
+PAST_FEATURES = DATA_COLS[1:]
+FUTURE_FEATURES = ['Future clearsky ghi', 'Future clearsky dni', 'Future clearsky dhi', 'Future Solar Eclipse Shading', 'Future zenith', 'Future elevation', 'Future azimuth']  # clear sky values are known ahead of time
+SCALAR_RESPONSES = ["GHI", "CSI GHI", "cs_dev t ghi"]
+RELATIVE_RESPONSES = ["Delta GHI", "Delta CSI GHI"]
+GROUP_FULL_NAMES = {}
+RESPONSE_FULL_NAMES = {
+    "GHI" : "Irradiance",
+    "Delta CSI GHI" : "Change in Clear Sky Index",
+    "Delta GHI" : "Change in Irradiance",
+    "cs_dev t ghi" : "Deviation from Clear Sky",
+    "CSI GHI" : "Clear Sky Index"
+}
+TIME_REPRESENTATION_ABBREVIATIONS = {
+    "Time Milestones" : "TM",
+    "Time of Year" : "ToY",
+    "Time of Day" : "ToD",
+    "Trig Time Milestones" : r"$\angle$ TM",
+    "Trig Time of Year" : r"$\angle$ ToY",
+    "Trig Time of Day" : r"$\angle$ ToD"
+}
+TARGET_REPRESENTATION_ABBREVIATIONS = {
+    "Irradiance" : "GHI",
+    "Clear Sky Index" : "CSI",
+    "Deviation from Clear Sky" : "CS Dev.",
+    "Change in Irradiance" : r"$\Delta$ GHI",
+    "Change in Clear Sky Index" : r"$\Delta$ CSI"
+}
