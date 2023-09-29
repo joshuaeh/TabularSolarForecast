0d8abda60d53434950c2b4a88d8e138069c2037c
joshuaeh <jhammond02@gmail.com>
2023-09-29 12:57:20 -0500

    scripts for creating a python environment

---
+++ create_env.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Script to create conda env for solarprophet on commputer with NVIDIA GPU
+# Author: Joshua Hammond
+
+# TODO check for conda install, and warn if not installed
+# create conda env
+echo "Creating conda env"
+conda create -q -n solarprophet python=3.8 -y
+conda activate solarprophet
+
+# install tensorflow
+printf  "Installing tensorflow"
+conda install -c conda-forge cudatoolkit=11.2 cudnn=8.1.0 -y -q
+pip install --upgrade pip -q
+# Anything above 2.10 is not supported on the GPU on Windows Native
+pip install "tensorflow<2.11" -q
+# check tensorflow installation with the following command:
+printf  "\nChecking TensorFlow installation. GPU should be listed below:"
+python -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'), '\n')"
+
+# install other required packages:
+printf  "Installing Other Packages"
+pip install -q pandas seaborn Pillow scipy dask statsmodels tqdm neptune neptune-tensorflow-keras ipykernel joblib scikit-learn Jinja2 --user --no-cache-dir --no-warn-script-location
+# pytables is a bit finnicky, needs to be installed and upgraded
+pip install -q --user --upgrade tables 
+
+printf  "\n\nComplete."
+printf  "Run 'conda activate solarprophet' to activate the environment."
+printf  "You may close this window if you like."
+bash
